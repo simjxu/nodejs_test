6ee462aa607ede4ed2c8f610c85dee78e2f3ee9f
Simon Xu <simon@petasense.com>
2018-05-15 22:14:34 -0700

    working with postman

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 
-// 'use strict';
-// var express = require('express');
-// var bodyParser = require('body-parser');
-
-// var app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// app.post('/todos', (req, res) => {
-// console.log(req.body);
-// });
-
-// app.listen(3000, () => {
-// console.log('Server is running on port 3000');
-// });
-
 'use strict';
 // List of the imported packages for this script
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+
+// MongoDB requireemnts for storage locally
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
+
+// // Firebase requirements for storage
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
+
+// admin.initializeApp(functions.config().firebase);
+// var db = admin.firestore();
 
 // Start express app
 var app = express();
@@ -33,33 +27,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Set Static Path
-app.use(express.static(path.join(__dirname, 'public')));
-
-var users = [
-	{
-		id:1,
-		first_name: 'John',
-		last_name: 'Doe',
-		email: 'johndoe@gmail.com'
-	}
-]
-
 app.get('/', function(req,res){
-	var title = 'Customers';
-	res.render('index', {title: 'Customers', users: users})
-})
+	db.users.find(function (err, docs)  {
+		console.log(docs);
 
-// app.post('/users/add', function(req,res){
-// // When you press submit on the form, this function is called
-// 	var newUser = {
-// 		first_name: req.body.first_name,
-// 		last_name: req.body.last_name,
-// 		email: req.body.email
-// 	}
-// 	console.log(newUser);
-// 	// console.log("hello, running app.post")
-// });
+		// Render the index.ejs file
+		res.render('index', {
+			title: 'Customers',
+			users: docs
+		});
+	})
+
+});
+
 
 // Define people variable
 var people = [
@@ -73,32 +53,50 @@ var people = [
 	}
 ]
 
-app.get('/receive', function(req,res){
-	// res.json(people);		// This just displays in json the json text you displayed above
-	// req.body;
+// app.get('/receive', function(req,res){
+// 	// res.json(people);		// This just displays in json the json text you displayed above
+// 	// req.body;
 
-	// // Option 2: Just use res.send to send a simple text
-	// res.send('Hello');  		// This just displays hello.
+// 	// Option 2: Just use res.send to send a simple text
+// 	res.send('Hello');  		// This just displays hello.
 
-	// Option 3: Render from a javascript file
-	console.log("test run")
-	res.render('index');
-});
+// 	// // Option 3: Render from a javascript file
+// 	// console.log("test run")
+// 	// res.render('index');
+// });
 
 app.post('/update', function(req,res){
-	// var package_body = [1,2,3,4]		// package body will the json file that you want to display on the page
-	// res.send(package_body)
-	// var json_pack = {
-	// 	key: req.body.Key,
-	// 	value: req.body.Value
-	// }
+	// The following lets me create a post via the submit button, but I can also use 
+	// postman to post into the database, as long as I have the first, last name, and 
+	// email.
+	// Example:
+	/*
+		{
+			"first_name": "abc",
+			"last_name": "def",
+			"email": "something@somewhere.com"
+		}
+	*/
 
-	// console.log(json_pack);
-	// console.log(req.body.Key);
-	// console.log("FORM SUBMITTED");
-	var keyName1=req.body.item;
-	console.log(keyName1);
-	res.send("nothing")
+	// This section is version for MongoDB
+	var newUser = {
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		email: req.body.email
+	}
+	db.users.insert(newUser, function(err, result){
+		if(err){
+			console.log(err);
+		}
+		res.redirect('/');
+	});
+	// var keyName1=req.body.item;
+	// console.log(keyName1);
+	// res.render(keyName1);
+	// res.send("nothing");
+
+	// // This section is the version for Firestore
+
 })
 
 
@@ -107,7 +105,7 @@ app.listen(3000, function(){
 	console.log('Server started on Port 3000...');
 })
 
-
+// // Another way to listen
 // var server = app.listen(process.env.PORT || '8080', function(){
 // 	console.log('App listening on port %s', server.address().port);
 // 	console.log('Press Ctrl+C to quit')
